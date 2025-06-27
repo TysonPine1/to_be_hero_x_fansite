09ac3acfeb470d77b4ceff8d7727466e0ca65dfb
Tyson Pine <kaungthawmaung91@gmail.com>
2025-06-27 20:50:30 +0700

    Complete character page, 1st result

--- js/script.js
+++ js/script.js
@@ -6,3 +6,30 @@ const toggle = document.getElementById('modeToggle');
     };
     setMode(localStorage.getItem('darkMode') === 'true');
     toggle.addEventListener('click', () => setMode(!document.body.classList.contains('dark')));
+
+//Modal 
+
+const modal = document.getElementById("modal");
+const modalTitle = document.getElementById("modal-title");
+const modalDesc = document.getElementById("modal-description");
+const closeBtn = document.querySelector(".close-btn");
+
+document.querySelectorAll(".hero-card").forEach(card => {
+  card.addEventListener("click", () => {
+    const name = card.querySelector(".hero-name").innerText;
+    const number = card.querySelector(".rank").innerText;
+    modalTitle.innerText = name;
+    modalDesc.innerText = `This is character No.${number}. More information will be added here.`;
+    modal.style.display = "flex";
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
